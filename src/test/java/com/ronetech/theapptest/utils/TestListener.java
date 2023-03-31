fb3564171102b60db668c68e71db12b64145bb40
rajivmanivannan <rajiv.techconsultant@gmail.com>
2023-03-31 15:58:19 +0530

    Fixed report path issue

--- src/test/java/com/ronetech/theapptest/utils/TestListener.java
+++ src/test/java/com/ronetech/theapptest/utils/TestListener.java
@@ -69,7 +69,7 @@ public class TestListener implements ITestListener {
 
     private String getScreenshotPath(String testCaseName, AppiumDriver driver) throws IOException {
         File source = driver.getScreenshotAs(OutputType.FILE);
-        String destinationFile = System.getProperty("user.dir") + "//reports" + testCaseName + ".png";
+        String destinationFile = System.getProperty("user.dir") + "/reports/" + testCaseName + ".png";
         FileUtils.copyFile(source, new File(destinationFile));
         return destinationFile;
     }
